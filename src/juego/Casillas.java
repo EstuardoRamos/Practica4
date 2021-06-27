6ba6e65ab964562ea5f6dc31caa1c77a4a618e0a
EstuardoRamos <estuardoramosg@gmail.com>
2021-06-27 01:28:39 -0600

    dibujando tablero

--- src/juego/Casillas.java
+++ src/juego/Casillas.java
@@ -2,8 +2,17 @@
 
 package juego;
 
+import javax.swing.JLabel;
+
 public class Casillas {
     private int numero;
     private String color;
+    private JLabel casill;
+   
+    
+    JLabel[][] label = new JLabel[5][5];
+    
+    
+    
 
 }
