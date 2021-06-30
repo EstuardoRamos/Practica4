7750e72e9e5b48010c64bb18fbf528d44c024329
EstuardoRamos <estuardoramosg@gmail.com>
2021-06-30 06:54:11 -0600

    agregando videos

--- src/practica4/Practica4.java
+++ src/practica4/Practica4.java
@@ -1,9 +1,13 @@
 package practica4;
 
+import ventanas.Principal;
+
 public class Practica4 {
 
     
     public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);
         
     }
     
